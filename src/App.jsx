a9624f88abf500b60c8173199958ccf07e355dc8
Krutik Bundela <krutikbundela2001@gmail.com>
2024-11-26 16:01:57 +0530

    Added Static Table component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
 import './App.css'
+import TableComponent from './Components/TableComponent'
 
 function App() {
 
   return (
     <>
-      
+      <TableComponent/>
     </>
   )
 }
